f490ca0b7229588308412807143bb0a918e7f66d
yse-yoo <youhei_yoshikawa@yse-c.net>
2024-09-18 10:55:57 +0900

    update

--- whats_photo.js
+++ whats_photo.js
@@ -7,13 +7,18 @@ const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
 const genAI = new GoogleGenerativeAI(GEMINI_API_KEY);
 
 async function run() {
-    const prompt = "What is in this photo?";
+    // プロンプト
+    const prompt = "この写真はなんですか？";
+
+    // LLMの選択「gemini-1.5-flash」
     const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+    // Gemini API にリクエスト
     const result = await model.generateContent([
         prompt,
         {
             inlineData: {
-                data: Buffer.from(fs.readFileSync('./images/photo1.jpg')).toString("base64"),
+                data: Buffer.from(fs.readFileSync('./images/photo2.jpg')).toString("base64"),
                 mimeType: 'image/jpeg'
             }
         }]
